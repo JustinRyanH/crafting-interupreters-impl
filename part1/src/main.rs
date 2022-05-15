4bda7668d72c00938b5087039e73f9923afc102a
Justin Hurstwright <jryanhurst@gmail.com>
2022-05-15 14:37:48 -0500

    init

--- part1/src/main.rs
+++ part1/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use core::fmt;
+use std::{env, error::Error};
+
+#[derive(Debug)]
+enum LuxError {
+    AppError(&'static str),
+}
+
+impl fmt::Display for LuxError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            LuxError::AppError(err) => write!(f, "AppError: {}", err),
+        }
+    }
+}
+
+impl Error for LuxError {}
+
+fn main() -> Result<(), LuxError> {
+    match env::args().skip(1).count() {
+        0 => Ok(()),
+        1 => Ok(()),
+        _ => {
+            println!("Usage: rlox [script]");
+            Err(LuxError::AppError("Too many arguments"))
+        }
+    }
 }
